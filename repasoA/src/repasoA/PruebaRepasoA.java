96715679a962373ce4d1b1a31a3b956b4da2b49d
JonBalles <jonatan.a.ballestero@gmail.com>
2023-08-07 12:25:04 -0300

    Agrego ejercicio Java

---
+++ repasoA/src/repasoA/PruebaRepasoA.java
@@ -0,0 +1,17 @@
+package repasoA;
+
+public class PruebaRepasoA {
+    
+  public static void main(String[] args) {
+        GuiaA.suma();
+       System.out.println("*------------*");
+        GuiaA.nombrando();
+        System.out.println("*------------*");
+        GuiaA.frase();
+        System.out.println("*------------*");
+       GuiaA.temperature();
+        System.out.println("*------------*");
+        GuiaA.variantes();
+    }
+
+}
